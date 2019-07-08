ee62b4d329250471dbd7cb14a4cb60726335c612
Rodrigo Botti <rodrigo.botti@gmail.com>
2019-07-08 03:49:13 -0300

    Improvement: replacing database worker pool implementation for :poolboy

--- lib/todo/database.ex
+++ lib/todo/database.ex
@@ -2,39 +2,39 @@ defmodule Todo.Database do
   @db_folder "./persist"
   @pool_size 3
 
-  def start_link do
-    IO.puts("Starting database server")
-    File.mkdir_p!(@db_folder)
-    children = Enum.map(1..@pool_size, &worker_spec/1)
-    Supervisor.start_link(children, strategy: :one_for_one)
-  end
-
   def store(key, data) do
-    key
-    |> choose_worker()
-    |> Todo.DatabaseWorker.store(key, data)
+    # asks pool for a single worker
+    :poolboy.transaction(
+      __MODULE__,
+      # invoked when worker is available
+      # once the lambda finishes, the worker is returned to the pool
+      fn worker_pid ->
+        Todo.DatabaseWorker.store(worker_pid, key, data)
+      end
+    )
   end
 
   def get(key) do
-    key
-    |> choose_worker()
-    |> Todo.DatabaseWorker.get(key)
+    :poolboy.transaction(
+      __MODULE__,
+      fn worker_id ->
+        Todo.DatabaseWorker.get(worker_id, key)
+      end
+    )
   end
 
   def child_spec(_) do
-    %{
-      id: __MODULE__,
-      start: {__MODULE__, :start_link, []},
-      type: :supervisor
-    }
-  end
+    File.mkdir_p!(@db_folder)
 
-  defp choose_worker(key) do
-    :erlang.phash2(key, @pool_size) + 1
-  end
-
-  defp worker_spec(worker_id) do
-    default_spec = {Todo.DatabaseWorker, {@db_folder, worker_id}}
-    Supervisor.child_spec(default_spec, id: worker_id)
+    :poolboy.child_spec(
+      __MODULE__, # child id
+      # pool options
+      [
+        name: {:local, __MODULE__}, # pool manager locally registered
+        worker_module: Todo.DatabaseWorker,
+        size: @pool_size
+      ],
+      [folder: @db_folder] # worker arguments passed to start_link
+    )
   end
 end
--- lib/todo/database_worker.ex
+++ lib/todo/database_worker.ex
@@ -2,17 +2,17 @@ defmodule Todo.DatabaseWorker do
   use GenServer
 
   # API
-  def start_link({folder, worker_id}) do
-    IO.puts("Starting database worker #{worker_id}")
-    GenServer.start_link(__MODULE__, folder, name: via_tuple(worker_id))
+  def start_link(folder: folder) do
+    IO.puts("Starting database worker")
+    GenServer.start_link(__MODULE__, folder)
   end
 
-  def store(worker_id, key, data) do
-    GenServer.cast(via_tuple(worker_id), {:store, key, data})
+  def store(pid, key, data) do
+    GenServer.cast(pid, {:store, key, data})
   end
 
-  def get(worker_id, key) do
-    GenServer.call(via_tuple(worker_id), {:get, key})
+  def get(pid, key) do
+    GenServer.call(pid, {:get, key})
   end
 
   # Server
@@ -22,9 +22,6 @@ defmodule Todo.DatabaseWorker do
     {:ok, folder}
   end
 
-  def via_tuple(worker_id) do
-    Todo.ProcessRegistry.via_tuple({__MODULE__, worker_id})
-  end
 
   @impl GenServer
   def handle_cast({:store, key, data}, folder) do
